32833b799124bae0bf835db729d6705b5c0460a3
Anto <anto2005antony@gmail.com>
2026-03-21 16:31:19 -0500

    main updates

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main(image_path, microscope_conversion_factor):
     valid_root_hair_masks, components_masks = makeValidRootHairMasks(skeletonized_hairs, contour, microscope_conversion_factor)
     root_hair_overlay = makeFinalMaskWithFinalRootHairs(image_grey, valid_root_hair_masks)
 
-    fig, ax = plt.subplots(3, 3, figsize=(30, 20))
+    fig, ax = plt.subplots(3, 3, figsize=(15,8))
     ax[0, 0].imshow(image_grey, cmap='gray')
     ax[0, 0].set_title('Original Grayscale Image')
     ax[0, 0].axis('off')
@@ -36,17 +36,17 @@ def main(image_path, microscope_conversion_factor):
     ax[0, 2].imshow(root_hair_mask, cmap='gray')
     ax[0, 2].set_title('Root Hair Mask (Better Adaptive Threshold - Closed Contour)')
     ax[0, 2].axis('off')
-    ax[1, 0].imshow(skeletonized_hairs, cmap='gray')
-    ax[1, 0].set_title('Skeletonized Root Hairs')
+    ax[1, 0].imshow(final_overlay, cmap='nipy_spectral')
+    ax[1, 0].set_title('Final_overlay')
     ax[1, 0].axis('off')
-    ax[1, 1].imshow(skeletonized_hairs_with_contours, cmap='gray')
-    ax[1, 1].set_title('Skeletonized Root Hairs with Contours')
+    ax[1, 1].imshow(skeletonized_hairs, cmap='gray')
+    ax[1, 1].set_title('Skeletonized Root Hairs')
     ax[1, 1].axis('off')
-    ax[1, 2].imshow(components_masks, cmap='nipy_spectral')
-    ax[1, 2].set_title('Labeled Components')
+    ax[1, 2].imshow(skeletonized_hairs_with_contours, cmap='gray')
+    ax[1, 2].set_title('Skeletonized Root Hairs with Contours')
     ax[1, 2].axis('off')
-    ax[2, 0].imshow(final_overlay, cmap='nipy_spectral')
-    ax[2, 0].set_title('Final_overlay')
+    ax[2, 0].imshow(components_masks, cmap='nipy_spectral')
+    ax[2, 0].set_title('Labeled Components')
     ax[2, 0].axis('off')
     ax[2, 1].imshow(root_hair_overlay, cmap='nipy_spectral')
     ax[2, 1].set_title('Final Labeled Components after filtering')
@@ -57,6 +57,6 @@ def main(image_path, microscope_conversion_factor):
     plt.show()
 
 if __name__ == "__main__":
-    image_path = "/Users/antoantony/9-30/KO/KO 10 um T0/KO 10 um_6.bmp"
+    image_path = "/Users/antoantony/9-30/KO/KO 10 um T0/KO 10 um_1.bmp"
     microscope_conversion_factor = 3.93626769
     main(image_path, microscope_conversion_factor)
