e02839e81b961e6c59c2629bcb94a78df7e1d6cc
Anto <anto2005antony@gmail.com>
2026-04-01 10:07:20 -0500

    rename files

--- streamlit_control.py
+++ streamlit_control.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 # main_page = st.Page("streamlit_main.py", title="Root Hair Analyzer")
-main_page = st.Page("streamlit_main_testing.py", title="Root Hair Analyzer")
+main_page = st.Page("streamlit_main.py", title="Root Hair Analyzer")
 # page_2 = st.Page("page_2.py", title="Background")
 
 pg = st.navigation([main_page])
--- streamlit_main.py
+++ streamlit_main.py
@@ -9,6 +9,14 @@ from main import *
 
 if 'conversion factor' not in st.session_state:
     st.session_state['conversion factor'] = 0.0000000000
+if 'image1' not in st.session_state:
+    st.session_state['image1'] = None
+if 'fig1' not in st.session_state:
+    st.session_state['fig1'] = None
+if 'image2' not in st.session_state:
+    st.session_state['image2'] = None
+if 'fig2' not in st.session_state:
+    st.session_state['fig2'] = None
 
 st.title('Root Hair Analyzer')
 
@@ -22,62 +30,96 @@ microscope_conversion_factor = st.number_input(
 
 st.session_state['conversion factor'] = microscope_conversion_factor
 
-### -----Image upload and display--------
-uploaded_file = st.file_uploader(
-    "Upload an image",
-    type=["bmp", "png", "jpg", "jpeg", "tif", "tiff"],
-    key='1'
-)
-uploaded_file.seek(0) # reset file pointer to the beginnign for next read 
+### -----Length parameters--------
 
-image_gray = None
-if uploaded_file is not None:
-    # file_bytes = np.asarray(bytearray(uploaded_file.getvalue()), dtype=np.uint8)
-    file_bytes = np.frombuffer(uploaded_file.read(), np.uint8)
-    image_gray = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
-    st.session_state['image1'] = image_gray
+column1, column2 = st.columns(2)
+with column1:
+    upper = st.number_input(
+        'Upper length threshold (in microns):', 
+        min_value=0.0, 
+        step=0.1, 
+        format='%.1f',
+        value= 100.0)
 
-    st.image(image_gray, caption="Grayscale image", )
+with column2:
+    lower = st.number_input(
+        'Lower length threshold (in microns):', 
+        min_value=0.0, 
+        step=0.1, 
+        format='%.1f',
+        value= 30.0)
 
-st.text('')
-st.text('')
-st.text('')
-st.text('')
-st.text('')
-st.text('')
-st.text('')
+## -----Image upload and analysis per column--------
+col1, col2 = st.columns(2)
 
-if st.button("Analyze", key='analysis1'):
-    fig1 = main(image_gray, microscope_conversion_factor, upper, lower)
-    fig1.update_layout(autosize = False,
-                    width = 1000,
-                    height = 800)
-    st.session_state['fig1'] = fig1
+with col1:
+    uploaded_file1 = st.file_uploader(
+        "Upload an image",
+        type=["bmp", "png", "jpg", "jpeg", "tif", "tiff"],
+        key='1'
+    )
 
-    st.plotly_chart(fig1, use_container_width=True)
+    image_gray1 = None
+    if uploaded_file1 is not None:
+        uploaded_file1.seek(0) # reset file pointer to the beginning  
+        # file_bytes2 = np.asarray(bytearray(uploaded_file2.getvalue()), dtype=np.uint8)
+        file_bytes1 = np.frombuffer(uploaded_file1.read(), np.uint8)
+        image_gray1 = cv2.imdecode(file_bytes1, cv2.IMREAD_GRAYSCALE)
+        st.session_state['image1'] = image_gray1
 
+        st.image(image_gray1, caption="Grayscale image", )
 
-'''
-User inputs a conversion factor that is saved to session state and stays that way 
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
 
-grayscale image is uploaded, then saved to session state
-If: the user uploads a NEW image (need to check if its new)
-- the session state updates
-- session state is presented in st.image()
-else:
-- the current session state is presented 
+    if st.button("Analyze", key='analysis1'):
+        imageToAnalyze = st.session_state['image1']
+        fig1 = main(imageToAnalyze, microscope_conversion_factor, upper, lower)
+        fig1.update_layout(autosize = False,
+                        width = 1000,
+                        height = 800)
+        st.session_state['fig1'] = fig1
 
-When user hits analyze
-If: there is an image in session state
-- analyze that image using main()
-- save the resulting fig to session state
-- if there is a present plotly figure
-    - display it 
-else:
-- error message that says 'no file uploaded' or something like that 
+    if st.session_state['fig1'] is not None:
+        st.plotly_chart(st.session_state['fig1'], use_container_width=True)
 
+with col2:
+    uploaded_file2 = st.file_uploader(
+        "Upload an image",
+        type=["bmp", "png", "jpg", "jpeg", "tif", "tiff"],
+        key='2'
+    )
+          
+    image_gray2 = None
+    if uploaded_file2 is not None:
+        uploaded_file2.seek(0) # reset file pointer to the beginning
+        # file_bytes2 = np.asarray(bytearray(uploaded_file2.getvalue()), dtype=np.uint8)
+        file_bytes2 = np.frombuffer(uploaded_file2.read(), np.uint8)
+        image_gray2 = cv2.imdecode(file_bytes2, cv2.IMREAD_GRAYSCALE)
+        st.session_state['image2'] = image_gray2
 
+        st.image(image_gray2, caption="Grayscale image", )
 
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
 
+    if st.button("Analyze", key='analysis2'):
+        imageToAnalyz2 = st.session_state['image2']
+        fig2 = main(imageToAnalyz2, microscope_conversion_factor, upper, lower)
+        fig2.update_layout(autosize = False,
+                        width = 1000,
+                        height = 800)
+        st.session_state['fig2'] = fig2
 
-'''
+    if st.session_state['fig2'] is not None:
+        st.plotly_chart(st.session_state['fig2'], use_container_width=True)
